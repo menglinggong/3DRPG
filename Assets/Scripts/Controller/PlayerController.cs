3487a54be6739b60a35d15ffcdb051e2067a1de0
menglinggong <60862504+menglinggong@users.noreply.github.com>
2023-10-22 13:50:18 +0800

    设置任务动画，添加切换动画方法

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -13,9 +13,15 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     private NavMeshAgent agent;
 
+    /// <summary>
+    /// 动画控制器
+    /// </summary>
+    private Animator animator;
+
     private void Awake()
     {
         agent = this.GetComponent<NavMeshAgent>();
+        animator = this.GetComponent<Animator>();
     }
 
     private void Start()
@@ -24,6 +30,18 @@ public class PlayerController : MonoBehaviour
         MouseManager.Instance.OnMouseClicked += MoveToTargetPos;
     }
 
+    private void Update()
+    {
+        SwitchAnimation(); 
+    }
+
+    /// <summary>
+    /// 控制玩家动画切换
+    /// </summary>
+    private void SwitchAnimation()
+    {
+        animator.SetFloat("Speed", agent.velocity.sqrMagnitude);
+    }
 
     /// <summary>
     /// 移动到目标点
@@ -31,6 +49,7 @@ public class PlayerController : MonoBehaviour
     /// <param name="targetpos"></param>
     private void MoveToTargetPos(Vector3 targetpos)
     {
+        
         agent.destination = targetpos;
     }
 }
